b704f299c4ee7c09f6d6d750d8d81caf91575225
yingliu4203 <yingliu4203@gmail.com>
2014-12-11 18:44:10 -0800

    fix dict value

--- amdeb_integrator/integrator/log_operation.py
+++ amdeb_integrator/integrator/log_operation.py
@@ -11,22 +11,23 @@ from ..shared.model_names import PRODUCT_OPERATION_TABLE
 def log_operation(env, operation_record):
     """ Log product operations. """
 
+    values = ''
     if 'values' in operation_record:
-        dumped_values = cPickle.dumps(
-            operation_record['values'], cPickle.HIGHEST_PROTOCOL)
+        values = operation_record['values']
+        dumped_values = cPickle.dumps(values, cPickle.HIGHEST_PROTOCOL)
         operation_record['operation_data'] = dumped_values
         operation_record.pop('values')
+        _logger.debug
 
     model = env[PRODUCT_OPERATION_TABLE]
     record = model.create(operation_record)
     logger_template = "Model: {0}, record id: {1}, template id: {2}. " \
-                      "{3} operation: {4}, record id {5}, values: {6}."
+                      "operation: {3}, record id: {4}, values: {5}."
     _logger.debug(logger_template.format(
-        operation_record.model_name,
-        operation_record.record_id,
-        operation_record.template_id,
-        PRODUCT_OPERATION_TABLE,
+        operation_record['model_name'],
+        operation_record['record_id'],
+        operation_record['template_id'],
         operation_record.operation_type,
         record.id,
-        operation_record.values
+        values
     ))
