a147be5309a82e8229207fac26612dc2f6e468c4
yingliu4203 <yingliu4203@gmail.com>
2015-01-12 21:21:19 -0800

    add product_sku field to product template and variant

--- amdeb_integrator/models/__init__.py
+++ amdeb_integrator/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import product_operation
+from . import odoo_product, product_operation
---
+++ amdeb_integrator/models/odoo_product.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# The SKU is probably used by more than one integrators
+# thus we add it here
+
+from openerp import models, fields
+from ..shared.model_names import PRODUCT_TEMPLATE_TABLE, PRODUCT_PRODUCT_TABLE
+
+
+class product_template(models.Model):
+    _inherit = [PRODUCT_TEMPLATE_TABLE]
+
+    # a template always has a SKU
+    product_sku = fields.Char(
+        string="Product SKU",
+        help="Product SKU is a unique identifier for each distinct product.",
+        index=True,
+    )
+
+
+class product_product(models.Model):
+    _inherit = [PRODUCT_PRODUCT_TABLE]
+
+    # only non-partial variant has a SKU, this field is hidden
+    # for partial variant
+    product_sku = fields.Char(
+        string="Product SKU",
+        help="Product SKU is a unique identifier for each distinct product.",
+        index=True,
+    )
